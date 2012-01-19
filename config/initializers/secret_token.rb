3dfe239b1dd275e7f59628679a9723a05a2a1061
ckhrysze <git@ckhrysze.net>
2012-01-18 21:23:33 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trellin::Application.config.secret_token = '0e5e1ebde814d31196130e601868a30a2804871c5a19019ed9207dad7971d1cdf754b2d9f096777f7dc02d1b27d1aba7d7956cb9101c1a87f62d4cf104a9c08d'
